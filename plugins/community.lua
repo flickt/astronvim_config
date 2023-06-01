55222a331d7c0d675239d8476a3f6ffe3bde6203
Taylor Flickinger <tayflick@gmail.com>
2023-06-01 14:29:11 -0600

    update colorscheme name

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,7 +4,7 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  { import = "astrocommunity.colorscheme.kanagawa" },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
   { import = "astrocommunity.test.neotest" },
