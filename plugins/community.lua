ed3f4061ffd4fd086ed831f2453526ce41f502ca
Taylor Flickinger <tayflick@gmail.com>
2023-07-06 16:56:00 -0600

    Remove github copilot

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,7 +5,7 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.colorscheme.kanagawa-nvim" },
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
   { import = "astrocommunity.test.neotest" },
 }
